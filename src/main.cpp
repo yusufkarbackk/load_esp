34bd1996ab991af92fa2b487dfbc63870192df47
Yusuf karback <yusufkarback64@gmail.com>
2023-06-03 22:43:12 +0700

    tambah kode mq135

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,21 @@ const int LOADCELL_SCK_PIN = 18;
 const int triggerPin = 5;
 const int echoPin = 35;
 
+#define placa "Arduino UNO"
+#define Voltage_Resolution 5
+#define pin A0                 // Analog input 0 of your arduino
+#define type "MQ-135"          // MQ135
+#define ADC_Bit_Resolution 10  // For arduino UNO/MEGA/NANO
+#define RatioMQ135CleanAir 3.6 // RS / R0 = 3.6 ppm
+// #define calibration_button 13 //Pin to calibrate your sensor
+
+MQUnifiedsensor MQ135(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
+
+char jenisgas[6][10] = {"CO", "Alcohol", "CO2", "Tolueno", "NH4", "Aceton"};
+float gasA[6] = {605.18, 77.255, 110.47, 44.947, 102.2, 34.668};
+float gasB[6] = {-3.937, -3.18, -2.862, -3.445, -2.473};
+int itemcheck = 4;
+
 HX711 scale;
 void setup()
 {
@@ -16,6 +31,37 @@ void setup()
 
   pinMode(triggerPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
+  // Set math model to calculate the PPM concentration and the value of constants
+  MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b
+  MQ135.setA(gasA[itemcheck]);
+  MQ135.setB(gasB[itemcheck]); // Configurate the ecuation values to get CO2 concentration
+  // MQ135.setA(102.2); MQ135.setB(-2.473); // Configurate the ecuation values to get NH4 concentration
+  MQ135.init();
+
+  Serial.print("Calibrating please wait.");
+  float calcR0 = 0;
+  for (int i = 1; i <= 10; i++)
+  {
+    MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
+    calcR0 += MQ135.calibrate(RatioMQ135CleanAir);
+    Serial.print(".");
+  }
+  MQ135.setR0(calcR0 / 10);
+  Serial.println("  done!.");
+  if (isinf(calcR0))
+  {
+    Serial.println("Warning: Conection issue founded, R0 is infite (Open circuit detected) please check your wiring and supply");
+    while (1)
+      ;
+  }
+  if (calcR0 == 0)
+  {
+    Serial.println("Warning: Conection issue founded, R0 is zero (Analog pin with short circuit to ground) please check your wiring and supply");
+    while (1)
+      ;
+  }
+  /**********/
 
   Serial.println("HX711 Demo");
   Serial.println("Initializing the scale");
@@ -60,7 +106,16 @@ void setup()
 
 void loop()
 {
-  //ultrasonic
+
+  MQ135.update();                   // Update data, the arduino will be read the voltage on the analog pin
+  float hasil = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+  Serial.print(jenisgas[itemcheck]);
+  Serial.print(" : ");
+  Serial.print(hasil);
+  Serial.println(" PPM");
+  delay(1000);
+
+  // ultrasonic
   long duration, jarak;
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
@@ -74,7 +129,7 @@ void loop()
   Serial.println(" cm");
   delay(1000);
 
-  //loadcell
+  // loadcell
   Serial.print("one reading:\t");
   Serial.print(scale.get_units(), 1);
   Serial.print("\t| average:\t");
